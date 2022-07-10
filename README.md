ec2a3c51434732a9e9055d809157ab556dd75856
Tauno Erik <sedumacre@gmail.com>
2022-07-10 11:26:03 +0300

    first blink test

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Electronic sculpture with machine vision
 
 ![PCB](doc/4N8A9532.jpg)
 
+
+## Links
+
+- https://docs.arduino.cc/hardware/nano-33-ble
+- https://docs.arduino.cc/tutorials/nano-33-ble-sense/cheat-sheet
+- https://community.element14.com/products/roadtest/b/blog/posts/review-of-development-board-arduino-nano-33-ble-sense-roadtest
  ___
 
 Copyright, 2022, Tauno Erik, [taunoerik.art](https://taunoerik.art). Made in :estonia:, Слава Україні! Героям Слава! :ukraine:
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <Arduino_APDS9960.h>  // Proximity and gesture sensor
+
+const int LED_LEFT = 7;
+const int LED_RIGHT = A7;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(LED_LEFT, OUTPUT);
+  pinMode(LED_RIGHT, OUTPUT);
+/*
+  if (!APDS.begin()) {
+    Serial.println("Error initializing APDS9960 sensor!");
+  }
+  */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /*
+  if (APDS.proximityAvailable()) {
+    Serial.println("Tere");
+    Serial.println(APDS.readProximity());
+  }*/
+  
+  digitalWrite(LED_LEFT, HIGH);
+  digitalWrite(LED_RIGHT, LOW);
+  delay(1000);
+
+  digitalWrite(LED_RIGHT, HIGH);
+  digitalWrite(LED_LEFT, LOW);
+  delay(1000);
+  
 }
