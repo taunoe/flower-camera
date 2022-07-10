436439540bd14951fa2f714cfc6f1bbd13bcf6ad
Tauno Erik <sedumacre@gmail.com>
2022-07-10 20:53:37 +0300

    example camera code

--- README.md
+++ README.md
@@ -4,12 +4,30 @@ Electronic sculpture with machine vision
 
 ![PCB](doc/4N8A9532.jpg)
 
+## OV7670 camera
+
+p. 163.
+
+Sice microcontrollers have limited memory, it is a good choice because it is capable of transferring low-resolution images.
+
+Output is QVGA (320x240) and QQVGA (160x120) pictures.
+
+Can encode images in different colour formats: RGB565, RGB444 and YUCbCr422.
+
+**NB! Pin layout may be different on different camera modules!**
+![Camera pins](doc/camera_pins.jpg)
++
+### Example codes
+
+- https://github.com/PacktPublishing/TinyML-Cookbook/blob/main/Chapter05/ArduinoSketches/01_camera_capture.ino
+- https://github.com/PacktPublishing/TinyML-Cookbook/blob/main/Chapter05/ArduinoSketches/02_camera_capture_qvga_rgb565.ino
 
 ## Links
 
 - https://docs.arduino.cc/hardware/nano-33-ble
 - https://docs.arduino.cc/tutorials/nano-33-ble-sense/cheat-sheet
 - https://community.element14.com/products/roadtest/b/blog/posts/review-of-development-board-arduino-nano-33-ble-sense-roadtest
+
  ___
 
 Copyright, 2022, Tauno Erik, [taunoerik.art](https://taunoerik.art). Made in :estonia:, Слава Україні! Героям Слава! :ukraine:
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,54 @@
+/*************************************************************
+ Documentation: https://github.com/taunoe/flower-camera
+ Started 09.07.2022
+ Tauno Erik 2022
+**************************************************************/
 #include <Arduino.h>
+#include "mbed.h"
 #include <Arduino_APDS9960.h>  // Proximity and gesture sensor
+#include <Arduino_OV767X.h>    // Camera
 
-const int LED_LEFT = 7;
-const int LED_RIGHT = A7;
+// Camera
+#define PRESSED     0
+#define BUTTON      p30  // A2
+
+static mbed::DigitalIn button(BUTTON);
+
+int bytes_per_frame;
+int bytes_per_pixel;
+
+uint8_t data[320 * 240 * 2];
+
+inline void rgb565_rgb888(uint8_t* in, uint8_t* out) {
+  uint16_t p = (in[0] << 8) | in[1];
+
+  out[0] = ((p >> 11) & 0x1f) << 3;
+  out[1] = ((p >> 5) & 0x3f) << 2;
+  out[2] = (p & 0x1f) << 3;
+}
+
+// LEDs
+#define LED_LEFT  p23  // 7
+#define LED_RIGHT p3   // A7
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115600);
+  while (!Serial);
+
+
+  if (!Camera.begin(QVGA, RGB565, 1)) {
+    Serial.println("Failed to initialize camera!");
+    while (1);
+  } else {
+    Serial.println("OV7670 Camera initialized!");
+  }
+
+  button.mode(PullUp);
+
+  bytes_per_pixel = Camera.bytesPerPixel();
+  bytes_per_frame = Camera.width() * Camera.height() * bytes_per_pixel;
+
+  Camera.testPattern();
 
   pinMode(LED_LEFT, OUTPUT);
   pinMode(LED_RIGHT, OUTPUT);
@@ -23,6 +66,7 @@ void loop() {
     Serial.println(APDS.readProximity());
   }*/
   
+  /*
   digitalWrite(LED_LEFT, HIGH);
   digitalWrite(LED_RIGHT, LOW);
   delay(1000);
@@ -30,5 +74,24 @@ void loop() {
   digitalWrite(LED_RIGHT, HIGH);
   digitalWrite(LED_LEFT, LOW);
   delay(1000);
+  */
+
+  if(button == PRESSED) {
+    Camera.readFrame(data);
+    uint8_t rgb888[3];
+
+    Serial.println("<image>");
+    Serial.println(Camera.width());
+    Serial.println(Camera.height());
+
+    for(int i = 0; i < bytes_per_frame; i+=bytes_per_pixel) {
+      rgb565_rgb888(&data[i], &rgb888[0]);
+      Serial.println(rgb888[0]);
+      Serial.println(rgb888[1]);
+      Serial.println(rgb888[2]);
+    }
+
+    Serial.println("</image>");
+  }
   
 }
